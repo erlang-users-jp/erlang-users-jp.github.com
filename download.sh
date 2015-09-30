0a46f98d4f615f766f0200f9aff754c95463829b
UENISHI Kota <kota@basho.com>
2015-10-01 00:35:38 +0900

    Update to 18.1

--- download.sh
+++ download.sh
@@ -7,7 +7,7 @@
 #2015-04-09 11:21  95446113   s3://download.basho.co.jp/otp/download/otp_win32_17.5.exe
 #2015-04-09 11:22  95622661   s3://download.basho.co.jp/otp/download/otp_win64_17.5.exe
 
-VERSION=18.0
+VERSION=18.1
 BASEURL=http://erlang.org/download
 
 FILES="otp_doc_html_${VERSION}.tar.gz otp_doc_man_${VERSION}.tar.gz otp_src_${VERSION}.readme otp_src_${VERSION}.tar.gz otp_win32_${VERSION}.exe otp_win64_${VERSION}.exe"
